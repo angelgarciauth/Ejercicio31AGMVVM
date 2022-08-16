38a5dc14ce496b5af0df0ffaa0248cffc069cb80
ang <ag888927@gmail.com>
2022-08-16 11:01:48 -0600

    Models

---
+++ Ejercicio31AGMVVM/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio31AGMVVM.Models
+{
+    public class Employee
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+        public string Position { get; set; }
+        public string Photo { get; set; }
+    }
+}
